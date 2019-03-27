0412afbf3e6201dc06053f0360336e3a20e6fd64
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-03-27 07:00:45 -0400

    Update listitem controller create function

--- app/controllers/listitem.js
+++ app/controllers/listitem.js
@@ -2,7 +2,14 @@ const Listitem = require('../models/listitem')
 
 // Create  and save a new listitem
 exports.create = (req,res) => {
-
+  const listitem = req.body
+  Listitem.create(listitem)
+    .then(listitem => {
+      res.json(201)
+    })
+    .catch(err => {
+      res.json(500)
+    })
 }
 
 // Retrieve and return all listitems
