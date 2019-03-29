f24396b36b22e1f0efe1995b378984c3cecc327b
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-03-29 06:42:12 -0400

    Refactor code

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+
 const port = process.env.PORT || 8000
+const middleware = require('./app/middleware')
 
 // create express app
 const app = express();
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extend: true}))
 
-// parse requests of content-type - application/json
-app.use(bodyParser.json())
+middleware.before(app)
 
-// configuring the database
-const dbConfig = require('./config/database')
+// // configuring the database
+const database = require('./config/database')
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/on-track-api-test'
 
 //Connect to database
-mongoose.connect(dbConfig.uri, {
-  useNewUrlParser: true
-}).then(() => {
-  console.log('Successfully connected to the database')
-}).catch((err) => {
-  console.log('Could not connect to the database. Exiting process...', err)
-  process.exit()
-})
+  mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true
+  }).then(() => {
+    console.log('Successfully connected to the database')
+  }).catch((err) => {
+    console.log('Could not connect to the database. Exiting process...', err)
+    process.exit()
+  })
+
 
 app.get('/',(req,res) => {
   res.json({'message': 'Welcome to the to-do app. Create a list of to dos.'})
@@ -33,6 +33,8 @@ app.get('/',(req,res) => {
 
 // Require item routes
 require('./app/routes/item.js')(app);
+
+middleware.after(app)
 
 // listen to requests
 app.listen(port, ()=> {
