9f069f36e70e1dc047e8155f58528cb152c8e9e2
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-02 11:01:31 -0400

    Update cors config

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -4,6 +4,11 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
+const corsPort = 8000
+const corsOptions = {
+  origin: process.env.CLIENT_ORIGIN || `http://localhost:${corsPort}`
+}
+
 const before = (app) => {
   app.use(cors())
   // parse requests of content-type - application/x-www-form-urlencoded
